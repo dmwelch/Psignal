28424922bcf9648b0ec1646b0a9c0499c0ddd3dd
Dave Welch <david.welch@monsanto.com>
2018-01-26 22:10:22 -0600

    First commit

---
+++ Assets/Psignal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using UnityEngine;
+
+public class Psignal : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb2d;
+	public Matrix signal;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		signal = GetComponent<Matrix> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+}
